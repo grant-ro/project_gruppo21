48fcde9d645b24fe25c5583c855f0781d0e8f66b
Roberta Granturco <r.granturco@studenti.unisa.it>
2024-12-08 23:18:02 +0100

    Add project documentation and source code files

---
+++ html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gestorepersistenzadati_2ejava_0',['GestorePersistenzaDati.java',['../_gestore_persistenza_dati_8java.html',1,'']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cgilmorst",
+  1: "cgor",
+  2: "cgor",
+  3: "cgilmst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions"
+};
+
